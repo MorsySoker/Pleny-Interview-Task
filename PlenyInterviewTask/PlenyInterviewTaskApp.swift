e7a7977cc127fb7460ce28d991a81e3da2cd70cf
Morsy Mohamed Elsokary <morsysoker@gmail.com>
2023-03-07 21:05:33 +0200

    Initial Commit

---
+++ PlenyInterviewTask/PlenyInterviewTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlenyInterviewTaskApp.swift
+//  PlenyInterviewTask
+//
+//  Created by MorsyElsokary on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlenyInterviewTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
