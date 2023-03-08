107f724997c3f0e11d188c868c35be1bc32b4337
Morsy Mohamed Elsokary <morsysoker@gmail.com>
2023-03-08 22:04:17 +0200

    add the model for a Tab

---
+++ PlenyInterviewTask/Scenes/Tabbar/Model/Tabs.swift
@@ -0,0 +1,31 @@
+//
+//  Tabs.swift
+//  PlenyInterviewTask
+//
+//  Created by MorsyElsokary on 08/03/2023.
+//
+
+import Foundation
+
+enum Tab: Int, Identifiable, CaseIterable {
+    
+    case home
+    case market
+    case offers
+    case gallary
+    case profile
+    
+    var id: Int {
+        self.rawValue
+    }
+    
+    var icons: String {
+        switch self {
+        case .home: return "home"
+        case .market: return "market"
+        case .offers: return "offers"
+        case .gallary: return "gallary"
+        case .profile: return "profile"
+        }
+    }
+}
